60df3fdcfb2aca2d9e365ba321bb0fdcb6ab7abb
湧川　惠大 <ywakikawa@fourmix.co.jp>
2024-05-23 17:28:23 +0900

    vue-routerの追加

---
+++ laravel-app/resources/js/router.js
@@ -0,0 +1,43 @@
+import { createRouter, createWebHistory } from 'vue-router'
+
+const routes = []
+
+routes.push(
+    {
+        path: '/login',
+        name: 'login',
+        component: () => import('Login.vue'),
+    },
+    {
+        path: '/register',
+        name: 'register',
+        component: () => import('Register.vue'),
+    },
+    {
+        path: '/todo',
+        name: 'todo',
+        component: () => import('Todo.vue'),
+    },
+)
+
+// 存在しないパスは全てTOPへ
+routes.push(
+    {
+        path: '/:catchAll(.*)',
+        redirect: '/'
+    }
+)
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes,
+})
+
+router.beforeEach((to, from) => {
+    // URL直接入力の場合はTOPへ
+    if (to.path !== '/' && from.name === undefined) {
+        return '/'
+    }
+})
+
+export default router
