8d9aafa6ad856022b525c5c39883d870432a9db8
hohno <hohno@fourmix.co.jp>
2024-06-06 18:04:59 +0900

    vueファイルの作成

--- laravel-app/resources/views/App.vue
+++ laravel-app/resources/views/App.vue
@@ -1,9 +1,12 @@
 <template>
-    <h1>Hello World</h1>
+    <TodoList/>
 </template>
 
 <script>
+import TodoList from "./TodoList.vue";
+
 export default {
+    components: {TodoList}
 
 }
 </script>
---
+++ laravel-app/resources/views/TodoList.vue
@@ -0,0 +1,7 @@
+<template>
+<h1>Todo List</h1>
+<form>
+<input type="text" name="addTodo">
+<button>追加</button>
+</form>
+</template>
